f943c0d463395110a1ca828edb6b04ed898a93db
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-02 19:32:16 +0530

    listing the topics for learning the shell script

---
+++ 02.varaible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+Name=Rajabhushanam.yeguru
+echo "My name is $Name"
+echo "My name is ${Name}"
+
+DATE=$(date +%F)
+echo "Hello today'S date is $DATE"
+
+#passing variable
+Name=$1
+Welcome=$2
+How=$3
+echo "Hello $1,$2,$3"
+
+echo "Hello ${name},${Welcome},${How}"
+
+#to know no of arguments passed and what are they ?
+echo "The no of arguments is : $#"
+echo "The arguments are: $@ "
