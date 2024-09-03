a0d400b3e7e7191817add20ea606b7472581b0a3
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-03 18:48:56 +0530

    listing the topics for learning the shell script

--- 05.array.sh
+++ 05.array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-languages=("Telugu" "Hindi" "English" "Tamil" "Kannada" "Malayalam" "Marrati")
-echo "The first language is: ${languages[0]}"
-echo "The 3rd language is: ${languages[2]}"
-echo "The 5th Language is: ${languages[4]}"
-echo "The languages are: ${languages[@]}"
-echo "The total no of languages are: ${languages[#]}"
+languages=("Telugu" "Hindi" "English" "Tamil" "Kannada" "Malayalam" "Marathi")
+echo "The first LANG is: ${languages[0]}"
+echo "The 3rd LANG is: ${languages[2]}"
+echo "The 5th LANG is: ${languages[4]}"
+echo "The LANG are: ${languages[@]}"
+echo "The total no of LANG are: ${languages[#]}"
