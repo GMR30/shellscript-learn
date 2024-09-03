f39f43b5b90574d5129d62e3dfcebaee86057aad
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-03 19:29:59 +0530

    listing the topics for learning the shell script

---
+++ 09.removal_apps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+userid=$(id -u)
+Date=$(date +"%F-%H-%M-%S")
+Log_File="$Date.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $userid -ne 0 ]
+then
+   echo "You are not root user,please run the script with root user"
+   exit 127
+fi
+validate(){
+   if [ $1 -ne 0 ]
+  then
+    echo -e  "$2 ..is $R FAILED $N "
+    exit 127
+  else
+    echo -e  "$2 ..is $G SUCCESS $N "
+   fi
+          }
+yum remove git -y &>>$Log_File
+validate $? "GIT is UN-Installation "
+
+yum remove java -y &>>$Log_File
+validate $? "JAVA UN-Installation"
+
+yum remove net-tools -y &>>$Log_File
+validate $? "NET-TOOLS UN-Installation"
+
+yum remove wget -y &>>$Log_File
+validate $? "WGET UN-Installation"
