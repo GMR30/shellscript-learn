593a988f8ecb1eb6940d13a14501e44cbddffbc6
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-05 18:23:44 +0530

    listing the topics for learning the shell script

---
+++ 11.remove_appl_for.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+userid=$(id -u)
+Date=$(date +"%F-%H-%M-%S")
+Log_File="$Date.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+if [ $userid -ne 0 ]
+then
+   echo "You are not root user,please run the script with root user"
+   exit 127
+fi
+validate(){
+   if [ $1 -ne 0 ]
+  then
+    echo -e "$2 ..is $R FAILED $N  "
+    exit 127
+  else
+    echo -e  "$2 ..is $G SUCCESS $N "
+   fi
+          }
+yum list installed
+for Package in $@  #required packages you can to the input
+ do
+  yum remove $Package &>>$Log_File
+  validate $? "$Package removed successfully"
+ done
