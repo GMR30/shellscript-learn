4726b8e723adf83539611e75b227c14a214051ca
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-11 10:26:44 +0530

    listing the topics for learning the shell script

---
+++ 16.remove_old_logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+Date=$(date +"%F-%H-%M-%S")
+Path=/home/centos/shellscript-learn
+Log_File="$Date.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+Input=$(find /home/centos/shellscript-learn  -name  "*.log" -type f -mtime +1)
+echo -e "$G output is$N :${Input}" &>>$Log_File
+while IFS= read line;
+do
+
+  echo -e "$R Deleting the line:$N $line " &>>$Log_File
+  rm -rf  $line
+done<<<"$Input"
