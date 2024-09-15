cd243b59b80ae5a87fde63c7c2e1ff4b2c5f1916
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-15 18:45:35 +0530

    listing the topics for learning the shell script

--- 16.remove_old_logs.sh
+++ 16.remove_old_logs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 Date=$(date +"%F-%H-%M-%S")
-Path=/home/centos/shellscript-learn
+directory=/home/centos/shellscript-learn
 Log_File="$Date.log"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-Input=$(find /home/centos/shellscript-learn  -name  "*.log" -type f -mtime +1)
+Input=$(find $directory  -name  "*.log" -type f -mtime +1)
 echo -e "$G output is$N :${Input}" &>>$Log_File
 while IFS= read line;
 do
---
+++ 17.threshold_value_alert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}'
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}' | cut -d "%" -f1
+
+Disk_threshold=20
+Disk_usage=$(df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6 " " $1}' )
+message=""
+while IFS= read line;
+do
+  usage=$(echo $line | cut -d "%" -f1)
+  partition=$(echo $line | cut -d " " -f2)
+   echo "usage: $usage"
+   echo "partition: $partition"
+  #echo -e "Disk_usage:$Disk_usage"
+  if [ $usage -ge $Disk_threshold ]
+  then
+      message+="High disk usage on $partition:$usage%\n "
+  fi
+done <<<"$Disk_usage"
+echo "message:$message"
+echo -e "$message" | mail -s "High Disk usage" raj@subbannadairy.com
