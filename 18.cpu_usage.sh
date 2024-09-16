bd20b41c5e5a48bbd0dd6b7446dfe304bd640a42
rajab <devarapalli.mrudulareddy@gmail.com>
2024-09-16 18:41:18 +0530

    listing the topics for learning the shell script

---
+++ 18.cpu_usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}'
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}' | cut -d "%" -f1
+
+cpu_threshold=5
+cpu_usage=$(top -bn1 | awk '/Cpu/ { print $2}' )
+#v=$cpu_usage
+#s=${v/\.*/}
+#echo $s
+message=""
+while IFS= read line;
+do
+   usage=$(echo $line)
+   echo "cpu-usage: $usage"
+   v=$usage
+   s=${v/\.*/}
+   echo $s
+  if [ $s -ge $cpu_threshold ]
+  then
+      message+="High cpu usage on: $s "
+  fi
+done <<<"$cpu_usage"
+echo "message:$message"
+echo -e "$message" | mail -s "High cpu usage" raj@subbannadairy.com
+
